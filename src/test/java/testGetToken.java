8fa6695b47be7f66b89d5e0c66dea51078b51af4
donjon <donjon@donjondeMacBook-Pro.local>
2019-04-08 22:21:24 +0800

    test

---
+++ src/test/java/testGetToken.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+
+public class testGetToken {
+
+    @Test
+    void testToken(){
+        RestAssured.given().log().all()
+                .queryParam("corpid","wwd6da61649bd66fea")
+                .queryParam("corpsecret","1JPyY9GvPLZfpvxEDjok-Xt_9v7HIBYJhZUoO6EgNGY")
+                .when().get("https://qyapi.weixin.qq.com/cgi=bin/gettoken")
+                .then().log().all().statusCode(404);
+    }
+}
